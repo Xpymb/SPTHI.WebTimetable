1eb791f179018b73f6e3a327dd7792e3363bc375
Xpymb <48682063+Xpymb@users.noreply.github.com>
2021-06-08 17:23:16 +0500

    Alpha

--- Controllers/CallbackController.cs
+++ Controllers/CallbackController.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using VkApiBot.Models;
 using VkNet.Model;
 using VkNet.Utils;
