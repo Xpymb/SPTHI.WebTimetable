67e2e96dfeecd4e1cc48bba9a6fcdc80681840f3
Xpymb <48682063+Xpymb@users.noreply.github.com>
2021-06-08 18:00:42 +0500

    asdsad

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace VkApiBot
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.ReadKey();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
