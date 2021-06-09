55ce5dd410e949ecc4add435fdbb6892be651c4c
Xpymb <48682063+Xpymb@users.noreply.github.com>
2021-06-09 21:22:47 +0500

    asdasd

--- VkApiBot/Models/Commands/HelloCommand.cs
+++ VkApiBot/Models/Commands/HelloCommand.cs
@@ -13,11 +13,11 @@ namespace VkApiBot.Models.Commands
 
         public override void Execute(Message message, VkApi client)
         {
-            var userId = message.UserId;
+            var userId = message.FromId;
 
             client.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams
             {
-                UserId = userId,
+                PeerId = userId,
                 RandomId = new Random().Next(Int32.MinValue, Int32.MinValue),
                 Message = Message
             });
