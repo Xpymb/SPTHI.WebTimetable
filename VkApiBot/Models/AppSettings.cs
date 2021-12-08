adb556027cadc42c920665158de12e7b07458444
Xpymb <48682063+Xpymb@users.noreply.github.com>
2021-12-08 15:14:44 +0500

    update

--- VkApiBot/Models/AppSettings.cs
+++ VkApiBot/Models/AppSettings.cs
@@ -4,10 +4,10 @@ namespace VkApiBot.Models
 {
     public static class AppSettings
     {
-        public static ulong GroupId { get; private set; } = 205071272;
-        public static string Token { get; private set; } = "dbfffc021ae9db88fdc08e36fb90e0f9b8fca5f9a65dadc50a25cf2dc11cf6e93367bc74a14aedcaeaea9";
+        public static ulong GroupId { get; private set; }
+        public static string Token { get; private set; }
         public static string ApiVersion { get; private set; } = "5.131";
-        public static string AdminId { get; private set; } = "207753605";
+        public static string AdminId { get; private set; }
         public static string CallbackForm { get; private set; } = "https://docs.google.com/forms/d/e/1FAIpQLSdSlKu181UUls4rd9vA5xEZrDw154NAi3zEQdlw_yVq6dF1tQ/viewform";
 
         public static void SetToken(string token)
